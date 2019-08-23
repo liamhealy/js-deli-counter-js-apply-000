335106f79504e6fb535fc5dee2f1f42357d5bb85
Liam Healy <lhealy0711@gmail.com>
2019-08-23 17:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function nowServing(line) {
       if(line[i] == line.length - 1) {
         currentLine += ` ${i}. ${line[i]}`;
       }
-      else {currentLine += ` ${i}. ${line[i]},`'
+      else {
+        currentLine += ` ${i}. ${line[i]},`'
+    
+      }    
     }
   }
 }
