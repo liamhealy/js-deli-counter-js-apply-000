7822b85093c29af2c6c2af68434f009c7695d6ea
Liam Healy <lhealy0711@gmail.com>
2019-08-23 17:06:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function takeANumber(line, customer) {
   line.push(customer);
   return `Welcome, ${customer}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line) {
+  if(line.length == 0) {
+    return `The line is currently empty`;
+  }
+}
