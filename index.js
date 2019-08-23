24c93671c7a650124a2721ba64bf74af7742b9a6
Liam Healy <lhealy0711@gmail.com>
2019-08-23 17:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(line, customer) {
 function nowServing(line) {
   var currentLine = `The line is currently `;
   if(line.length == 0) {
-    return currentLine;
+    return currentLine + `empty.`;
   }
   else {
     
